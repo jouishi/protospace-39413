9200cae302e9a9703002c93e43bd3a17e2a215a2
jouishi <08251023jke01@gmail.com>
2023-06-12 00:11:38 +0900

    nine commit

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -43,13 +43,15 @@ class PrototypesController < ApplicationController
   end
 
   def destroy
-    prototype = Prototype.find(params[:id])
-    prototype.destroy
-    redirect_to root_path
-
-
+    @prototype = Prototype.find(params[:id])
+    
+    if @prototype.user == current_user
+      @prototype.destroy
+      redirect_to root_path
+    else
+      redirect_to root_path
+    end
   end
-
 
 
   
